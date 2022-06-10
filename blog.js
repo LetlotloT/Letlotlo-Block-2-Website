f712c53886f1be99b49c3eafb229fc498338f44f
LetlotloT <100612156+LetlotloT@users.noreply.github.com>
2022-06-10 18:08:10 +0200

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,56 @@
+function doit() {
+    document.getElementById("Week2Btn").style.textDecoration = 'underline'
+
+}
+
+function undo() {
+    document.getElementById("Week2Btn").style.textDecoration = 'none'
+};
+
+function doit4() {
+    document.getElementById("Week4Btn").style.textDecoration = 'underline'
+
+};
+
+function undo4() {
+    document.getElementById("Week4Btn").style.textDecoration = 'none'
+};
+
+function doit6() {
+    document.getElementById("Week6Btn").style.textDecoration = 'underline'
+
+}
+
+function undo6() {
+    document.getElementById("Week6Btn").style.textDecoration = 'none'
+}
+
+function doit9() {
+    document.getElementById("Week9Btn").style.textDecoration = 'underline'
+
+}
+
+function undo9() {
+    document.getElementById("Week9Btn").style.textDecoration = 'none'
+}
+
+function doit12() {
+    document.getElementById("Week12Btn").style.textDecoration = 'underline'
+
+}
+
+function undo12() {
+    document.getElementById("Week12Btn").style.textDecoration = 'none'
+}
+
+
+function doit14() {
+    document.getElementById("Week14Btn").style.textDecoration = 'underline'
+
+}
+
+function undo14() {
+    document.getElementById("Week14Btn").style.textDecoration = 'none'
+}
+
+
